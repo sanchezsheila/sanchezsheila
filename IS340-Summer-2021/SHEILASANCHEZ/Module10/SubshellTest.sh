9574f4c7656ae8568c904565aed2c8985758ad62
Sheila Sanchez <sanchezsheila@cityuniversity.edu>
2021-09-11 19:32:02 -0400

    Submission for Module 10

---
+++ IS340-Summer-2021/SHEILASANCHEZ/Module10/SubshellTest.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+sum=0
+printf "Add sum to these numbers:\n"
+printf "%s\n" ${RANDOM}{,,,} |
+while read num
+do
+  printf "The current number is %d\n" "$num"
+  ((sum=sum+num))
+  printf  "The current sum number is %d\n" "$num"
+done
+
+printf "Now the loop is finished.\n\nThe final sum is %d\n" "$sum"
+
